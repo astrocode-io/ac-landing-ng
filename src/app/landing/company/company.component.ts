d8e4671f389dff81bc0872d6df6d4ef3b13408c5
Rodrigo Gomez Villarroel <rodrigv31@gmail.com>
2020-12-08 09:28:27 -0300

    Added lottie animations

--- src/app/landing/company/company.component.ts
+++ src/app/landing/company/company.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 @Component({
   selector: 'landing-company',
   templateUrl: './company.component.html',
   styleUrls: ['./company.component.css'],
 })
 export class CompanyComponent {
-  constructor() {}
+  constructor() { }
+  options: AnimationOptions = {
+    path: '../../../assets/animations/drawkit-grape-animation-4-LOOP.json',
+  };
+
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
 }
